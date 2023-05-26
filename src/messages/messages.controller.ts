5c9f56ab85f16cf4ce4b79cbfbd6a5095fcb5a63
jaydeep4321 <jaydeepvaghela.thetatechnolabs@gmail.com>
2023-05-26 16:22:32 +0530

    updated form office

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,36 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
 import { createMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
   @Get()
-  listMessage() {}
+  listMessage() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: createMessageDto) {
-    console.log(body);
+    return this, this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
